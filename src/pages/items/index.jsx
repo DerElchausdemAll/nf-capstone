9aac53282cd9a358e81c8b6c1c220fb0eae1f8d1
Jan Peter <jan.peter92@mail.de>
2022-02-03 10:24:41 +0100

    feat: added bookmarks

--- src/pages/items/index.jsx
+++ src/pages/items/index.jsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import React from "react";
 import Layout from "../../organisms/layout";
 import Notes from "../../molecules/notes/Notes";
-import { Button } from "@mui/material";
 
 const Page = () => {
 	return (
