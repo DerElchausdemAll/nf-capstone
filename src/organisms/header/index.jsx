466ee2eb9daa748e100e12182024c02c4dc8436f
Jan Peter <jan.peter92@mail.de>
2022-02-07 17:20:53 +0100

    feat: added a navbar with icons

--- src/organisms/header/index.jsx
+++ src/organisms/header/index.jsx
@@ -1,23 +1,11 @@
-import Link from "next/link";
 import React from "react";
 
 const Header = () => {
 	return (
 		<header>
-			<nav
-				data-test-id="navigation"
-				style={{
-					display: "flex",
-					justifyContent: "space-evenly",
-					marginBottom: "20px",
-					marginTop: "20px",
-				}}
-			>
-				<Link href="/">Home</Link>
-				<Link href="/items">Items</Link>
-				<Link href="/profile">Profile</Link>
-				<Link href="/form">Form</Link>
-			</nav>
+			<div>
+				<h3 style={{ color: "white" }}>Hier entsteht ein Header</h3>
+			</div>
 		</header>
 	);
 };
