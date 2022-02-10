bc82259ac16f9313d1571013098be042f9a21ed6
Jan Peter <jan.peter92@mail.de>
2022-02-10 22:57:06 +0100

    chore: bug fixing

--- src/pages/test/[id].js
+++ src/pages/test/[id].js
@@ -1,14 +1,14 @@
-import React from "react";
-import Layout from "../../organisms/layout";
+// import React from "react";
+// import Layout from "../../organisms/layout";
 
-const Details = () => {
-	return (
-		<Layout>
-			<div>
-				<h1>Huhuu</h1>
-			</div>
-		</Layout>
-	);
-};
+// const Details = () => {
+// 	return (
+// 		<Layout>
+// 			<div>
+// 				<h1>Huhuu</h1>
+// 			</div>
+// 		</Layout>
+// 	);
+// };
 
-export default Details;
+// export default Details;
