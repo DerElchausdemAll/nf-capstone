7659fa429ca66842233e91e3c5f1aa9c2f411ed7
Jan Peter <jan.peter92@mail.de>
2022-02-20 20:14:37 +0100

    chore: changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+	env: {
+		MONGODB_URI: process.env.MONGODB_URI,
+	},
 };
 
 module.exports = nextConfig;
