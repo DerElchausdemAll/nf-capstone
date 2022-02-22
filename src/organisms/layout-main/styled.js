7a2af1923d18ce4b6037b9f42db7893720d1722d
Jan Peter <jan.peter92@mail.de>
2022-02-22 21:20:50 +0100

    feat: a lot of styling

--- src/organisms/layout-main/styled.js
+++ src/organisms/layout-main/styled.js
@@ -12,10 +12,10 @@ export const StyledBackground = styled.div`
 		content: "";
 		position: absolute;
 		z-index: -1;
-		width: 100%;
-		height: 100%;
 		top: 0;
 		left: 0;
+		width: 100%;
+		height: 100%;
 		background: url("https://images.unsplash.com/photo-1559149251-e9a1dc89981d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80");
 		background-position: 76% 100%;
 		background-size: cover;
