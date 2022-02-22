ae9239aaf200ace5e589e89404ae3313a7a6d580
Jan Peter <jan.peter92@mail.de>
2022-02-22 21:19:26 +0100

    feat: a lot of styling

--- src/organisms/layout-main/styled.js
+++ src/organisms/layout-main/styled.js
@@ -10,13 +10,13 @@ export const StyledBackground = styled.div`
 
 	&::before {
 		content: "";
-		background: url("https://images.unsplash.com/photo-1559149251-e9a1dc89981d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80");
 		position: absolute;
-		background-position: 76% 100%;
-		top: 0;
-		background-size: cover;
-		left: 0;
+		background: url("https://images.unsplash.com/photo-1559149251-e9a1dc89981d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80");
 		z-index: -1;
+		top: 0;
+		left: 0;
+		background-position: 76% 100%;
+		background-size: cover;
 		width: 100%;
 		height: 100%;
 		filter: brightness(0.98);
